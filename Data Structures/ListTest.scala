5bfb9b51a7476f4ed95a38023bd91bc8b9a32209
David <david.espinosa.21@gmail.com>
2021-04-08 14:08:27 -0500

    Create ListTest.scala

---
+++ Data Structures/ListTest.scala
@@ -0,0 +1,46 @@
+package com.revature.introdemos.datastructures
+
+/**
+  * A List is a linked list under the hood. So it is probably like LinkedList in Java.
+  * It is immutable, like strings, so you can only create new Lists using old Lists.
+  * 
+  * Initialize:
+  *     val ints = List(1, 2, 3)
+  *     val names = List("Joel", "Chris", "Ed")
+  * 
+  *     or
+  * 
+  *     val ints: List[Int] = List(1, 2, 3, 4)
+  * 
+  * Adding elements:
+  *     (a new list has to be created, so you technically can't really add to one, 
+  *      but you can make a new one)
+  * 
+  *     val b = 0 +: ints
+  *     val c = -1 +: b
+  *     val d = -2 +: c
+  * 
+  * More info: https://docs.scala-lang.org/overviews/scala-book/list-class.html
+  */
+object ListTest {
+    def run() : Unit = {
+
+        val ints = List(1, 2, 3, 4, 5)  // a linked list
+
+        val b = 0 +: ints
+        val c = -1 +: b
+        val d = -2 +: c
+
+        println(ints)
+        println(b)
+        println(c)
+        println(d)
+
+
+        // another way to declare a list
+        val list = -1 :: 0 :: 1 :: 2 :: 3 :: Nil
+
+        println(list)  // comes out as a List type
+
+    }
+}
